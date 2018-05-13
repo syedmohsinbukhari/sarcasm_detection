7eb020f5a90ed71151d8b5af74de6dcfdfab833c
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2018-05-13 13:07:29 +0500

    data_user_wise.py is ready

--- scripts/data_user_wise.py
+++ scripts/data_user_wise.py
@@ -17,14 +17,30 @@ setup_logging('logs/data_user_wise.log')
 logging.info("Running script data_user_wise.py")
 
 """--------------------------------------------------"""
+import json
+import os
 
 fdname = 'data/main/final_data.json'
 funame = 'data/main/user_wise.json'
 
-big_dict = {}
+user_dict = {}
 
-with open(fdname) as f_data, open(funame, 'wb') as f_user:
+with open(fdname) as f_data, open(funame, 'w') as f_user:
     for line in f_data:
-        logging.info("")
+        cmnt_dict = json.loads(line)
+        cmnt_id = list(cmnt_dict.keys())[0]
+        cmnt_author = cmnt_dict[cmnt_id]['author']
+
+        if not cmnt_author in user_dict.keys():
+            user_dict[cmnt_author] = {'comment_count': 0}
+
+        user_dict[cmnt_author][cmnt_id] = {
+            'text': cmnt_dict[cmnt_id]['text'],
+            'label': cmnt_dict[cmnt_id]['label']
+        }
+        user_dict[cmnt_author]['comment_count'] += 1
+
+    json_dump = json.dumps(user_dict)
+    f_user.write(json_dump)
 
 logging.info("Finished script data_user_wise.py")

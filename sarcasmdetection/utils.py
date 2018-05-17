54bc09d8fcf4dc179c9e552caa3e0c735398ef51
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2018-05-17 21:05:06 +0500

    providing log file name is now optional in utils.py

--- sarcasmdetection/utils.py
+++ sarcasmdetection/utils.py
@@ -3,17 +3,18 @@ import logging
 from os import listdir
 from os.path import isfile, join
 
-def setup_logging(log_fname):
+def setup_logging(log_fname=''):
     rootLogger = logging.getLogger()
     rootLogger.setLevel(logging.DEBUG)
 
-    fileHandler = logging.FileHandler("{0}".format(log_fname))
-    fileFormatter = logging.Formatter("%(asctime)s [%(filename)s:%(lineno)s " +
-                                      "- %(funcName)s() ] [%(levelname)s] " +
-                                      "%(message)s")
-    fileHandler.setFormatter(fileFormatter)
-    fileHandler.setLevel(logging.INFO)
-    rootLogger.addHandler(fileHandler)
+    if log_fname != '':
+        format_string = "%(asctime)s [%(filename)s:%(lineno)s - "+
+                        "%(funcName)s() ] [%(levelname)s] %(message)s"
+        fileHandler = logging.FileHandler("{0}".format(log_fname))
+        fileFormatter = logging.Formatter(format_string)
+        fileHandler.setFormatter(fileFormatter)
+        fileHandler.setLevel(logging.INFO)
+        rootLogger.addHandler(fileHandler)
 
     consoleHandler = logging.StreamHandler()
     consoleFormatter = logging.Formatter("[%(levelname)s]  %(message)s")
